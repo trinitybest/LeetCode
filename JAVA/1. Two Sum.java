e5b06fbfb71113b149679ca20819877becd2e5db
Dean <hutianyou6@gmail.com>
2020-05-30 11:51:43 +1200

    Update 1. Two Sum.java

--- JAVA/1. Two Sum.java
+++ JAVA/1. Two Sum.java
@@ -12,6 +12,23 @@ class Solution {
     }
     
 }
+// Approach 2: Two-pass Hash Table
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[] {map.get(complement), i};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    
+}
 
 // Approach 3: One-pass Hash Table
 class Solution {

091441679fecf011943ae5e1d842e3e61319c535
Dean <hutianyou6@gmail.com>
2020-05-30 11:41:22 +1200

    Update 1. Two Sum.java

--- JAVA/1. Two Sum.java
+++ JAVA/1. Two Sum.java
@@ -12,3 +12,23 @@ class Solution {
     }
     
 }
+
+// Approach 2: Two-pass Hash Table
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement)){
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    
+}
+
+/* take home notes
+* HashMap is like Python dict
+*/
